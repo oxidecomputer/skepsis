6eedd774c4fb35591018198807c48d29c7ddee1f
David Crespo <david.crespo@oxidecomputer.com>
2026-04-22 13:10:55 -0500

    add MPL 2.0

--- server/diff.ts
+++ server/diff.ts
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright Oxide Computer Company
+ */
+
 import { spawn } from 'node:child_process'
 import type { DiffArgs, FileHashes } from '../shared/types.ts'
 
--- server/main.ts
+++ server/main.ts
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright Oxide Computer Company
+ */
+
 import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
 import { serveStatic } from '@hono/node-server/serve-static'
